6738f8e5542bfbeff4903145e98339acd0849689
diego@alpha <diegorodriguezriera@gmail.com>
2019-04-21 23:42:36 +0200

    updated diagram

--- server/src/Controller/TrainController/TrainController.cpp
+++ server/src/Controller/TrainController/TrainController.cpp
@@ -1,33 +1,31 @@
 #include "TrainController.hpp"
 
 
-Train TrainController::getNextTrain()
+Train TrainController::getNextTrain(std::string station)
+{
+    time_t now = time(NULL);
+    struct tm now_tm = * localtime(&now);
+    return getLastTrainBefore(now_tm.tm_hour, now_tm.tm_min, station);
+}
+
+
+Train TrainController::getLastTrainBefore(int hour, int min, std::string station)
 {
     return Train();
 }
 
 
-Train TrainController::getLastTrainBefore(time_t time)
+Train TrainController::getNextTrainFrom(int hour, int min, std::string station)
 {
     return Train();
 }
 
 
-Train TrainController::getLastTrainBefore(tm time)
+std::vector<Train> TrainController::getTodayTrains(std::string station)
 {
-    return Train();
-}
-
-
-Train TrainController::getNextTrainFrom(time_t time)
-{
-    return Train();
-}
-
-
-Train TrainController::getNextTrainFrom(tm time)
-{
-    return Train();
+    std::vector<Train> trains;
     
+    
+    
+    return trains;
 }
-
--- server/src/Controller/TrainController/TrainController.hpp
+++ server/src/Controller/TrainController/TrainController.hpp
@@ -1,16 +1,17 @@
 #ifndef _PANCHO_TRAIN_CONTROLLER
 #define _PANCHO_TRAIN_CONTROLLER
 
-#include <ctime>
+#include <time.h>
+#include <string>
+#include <vector>
 #include "Train.cpp"
 
 namespace TrainController
 {
-    Train getNextTrain();
-    Train getLastTrainBefore(time_t time);
-    Train getLastTrainBefore(tm time);
-    Train getNextTrainFrom(time_t time);
-    Train getNextTrainFrom(tm time);
+    Train getNextTrain(std::string station);
+    Train getLastTrainBefore(int hour, int minute, std::string station);
+    Train getNextTrainFrom(int hour, int minute, std::string station);
+    std::vector<Train> getTodayTrains(std::string station);
 }
 
 #endif
