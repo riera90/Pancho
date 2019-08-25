d3db4e965a4fc954b31d7dd1c8e1cd7222f9e026
diego@alpha <diegorodriguezriera@gmail.com>
2019-08-25 20:59:50 +0200

    improved stability in alarm module

--- modules/alarm_clock/software/alarm.py
+++ modules/alarm_clock/software/alarm.py
@@ -32,7 +32,8 @@ from pprint import pprint
 ################################################################################
 
 ALARMS_XML = "alarms.xml"
-SNOOZ_TIME = timedelta(seconds=10)
+SNOOZE_TIME = timedelta(seconds=5)
+REPEAT_NUMBER = 3
 ALARM_MUSIC = "music.flac"
 
 HOSTNAME = "alarm"
@@ -49,7 +50,7 @@ MQTT_QOS = 0
 ################################################################################
 
 next_alarm = None
-alarm_is_ringing = False
+repeat_counter = 0
 
 # sets up the MQTT client
 mqtt_client = mqtt.Client(HOSTNAME)
@@ -58,22 +59,38 @@ mqtt_client.connect(MQTT_BROKER, MQTT_BROKER_PORT)
 mqtt_client.subscribe(MQTT_TOPIC_NIGHTSTAND_BUTTON, MQTT_QOS)
 
 
+def stop_alarm():
+    '''
+    stops the alarm, sending a stop signal to the speakers and stops the snooze functionality
+    '''
+    global repeat_counter
+    global next_alarm
+    print("stopping the alarm")
+    repeat_counter = 0
+    next_alarm = None
+    mqtt_client.publish(MQTT_TOPIC_SPEAKERS, 'stop', MQTT_QOS)
+
+
+def snooze_alarm():
+    '''
+    snoozes the alarm, sending a stop signal to the speakers and letting the snooze functionality do its job
+    '''
+    print("snoozing the alarm")
+    mqtt_client.publish(MQTT_TOPIC_SPEAKERS, 'stop', MQTT_QOS)
+
+
 # Define event callbacks
 def on_connect(client, userdata, flags, rc):
     print("rc: " + str(rc))
 
 def on_message(client, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    global alarm_is_ringing
 
     request = msg.payload.decode("utf-8")
     if request == 'hold':
-        print("stopping the alarm")
-        alarm_is_ringing = False
-        mqtt_client.publish(MQTT_TOPIC_SPEAKERS, 'stop', MQTT_QOS)
+        stop_alarm()
     if request == 'press':
-        print("snoozing the alarm")
-        mqtt_client.publish(MQTT_TOPIC_SPEAKERS, 'stop', MQTT_QOS)
+        snooze_alarm()
 
 def on_publish(client, obj, mid):
     print("mid: " + str(mid))
@@ -91,82 +108,88 @@ mqtt_client.on_publish = on_publish
 mqtt_client.on_subscribe = on_subscribe
 
 
-def time_plus(time, timedelta):
-    dummy_date = datetime.today()
-    full_datetime = datetime.combine(dummy_date, time)
-    added_datetime = full_datetime + timedelta
-    return added_datetime.time()
-
-
-def time_minus(time, timedelta):
-    dummy_date = datetime.today()
-    full_datetime = datetime.combine(dummy_date, time)
-    added_datetime = full_datetime - timedelta
-    return added_datetime.time()
-
-
-def get_next_alarm_for_today():
+def get_formatted_alarms():
     '''
-    Gets the next alarm (from the XML) of the actual day
-    :return: time / None
+    Gets the alarms (from the XML)
+    :return: datetime array / None
     '''
     tree = ET.parse(ALARMS_XML)
     root = tree.getroot()
     alarms = root.findall('alarm')
 
-    today_alarms = []
-    # from all the alarms
+    formatted_alarms = []
+    # builds the datetime from the xml for ease of comparision
+    today = datetime.today()
     for alarm in alarms:
-        # get today's alarms
-        if int(alarm.find('day').text) == datetime.now().weekday():
-            today_alarms.append(alarm)
+        alarm_time = datetime.strptime(alarm.find('time').text, "%H:%M:%S")
+        formatted_alarm = datetime(year=today.year,
+                                   month=today.month,
+                                   day=today.day,
+                                   hour=alarm_time.hour,
+                                   minute=alarm_time.minute,
+                                   second=alarm_time.second)
+        days_difference = (int)(alarm.find('day').text) - today.weekday()
+        if days_difference is 0: # the alarm is the same day as today
+            if datetime.strptime(alarm.find('time').text, "%H:%M:%S").time() <= datetime.now().time(): # the alarm has passed
+                days_difference = 7
+            else: # the alarm is today and it has not passed yet
+                days_difference = 0
+        else:
+            if days_difference < 0: # the alarm is in next week
+                # 0 1 2 3 4 5 6
+                # a           t
+                # days = 0 - 6 = -6
+                # days_diff = 7 + (- 6) = 1
+                days_difference = 7 + days_difference
 
-    # remove the already rang alarms
-    for alarm in today_alarms:
-        alarm_time = datetime.strptime(alarm.find('time').text, "%H:%M:%S").time()
-        if alarm_time <= datetime.now().time():
-            today_alarms.remove(alarm)
+            # else: the alarm is this week, useless for program but useful for programmer, so commented
+                # 0 1 2 3 4 5 6
+                #     t       a
+                # days = 6 - 2 = 4
+                # days_diff = 4
+                # days_difference = days_difference
 
-    # get the next alarm
-    next_alarm = None
-    for alarm in today_alarms:
-        alarm_time = datetime.strptime(alarm.find('time').text, "%H:%M:%S").time()
-        if next_alarm == None or alarm_time <= datetime.strptime(next_alarm.find('time').text, "%H:%M:%S").time():
+        difference = timedelta(days=days_difference)
+        formatted_alarm = formatted_alarm + difference
+        formatted_alarms.append(formatted_alarm)
+
+    return formatted_alarms
+
+
+def get_next_alarm():
+    '''
+    gets the next alarm from the returned by get_formatted_alarms function
+    :return: datetime
+    '''
+    alarms = get_formatted_alarms()
+    next_alarm = alarms[0]
+    for alarm in alarms:
+        if alarm < next_alarm:
             next_alarm = alarm
-
-    # return the next alarm for the day
-    if next_alarm is None:
-        return None
-    else:
-        return next_alarm
+    return next_alarm
 
 
 def ring_alarm():
     '''
     rings the mqtt alarm until the alarm is stopped or it timeouts
     '''
-    print("ring rang rung!")
-    global alarm_is_ringing
-    alarm_is_ringing = True
+    global repeat_counter
+    global next_alarm
 
-    last_ring = datetime.now().time()
+    repeat_counter += 1
+    print("ringing alarm:", next_alarm)
+    print("repaet no", repeat_counter)
+    mqtt_client.publish(MQTT_TOPIC_SPEAKERS, "play " + ALARM_MUSIC, MQTT_QOS)
 
-    mqtt_client.publish(MQTT_TOPIC_SPEAKERS, "play "+ALARM_MUSIC, MQTT_QOS)
+    if repeat_counter >= REPEAT_NUMBER: # the repeat counter has been already reached, canceling snooze functionality
+        next_alarm = None
+        repeat_counter = 0
+        print("EOA")
+        return
 
-    while alarm_is_ringing:
-        try:
-            mqtt_client.loop()
-            if time_plus(last_ring, SNOOZ_TIME) < datetime.now().time():
-                mqtt_client.publish(MQTT_TOPIC_SPEAKERS, "play "+ALARM_MUSIC, MQTT_QOS)
-                last_ring = datetime.now().time()
-        except Exception as e:
-            print(e)
-
-    mqtt_client.publish(MQTT_TOPIC_SPEAKERS, "stop", MQTT_QOS)
-
-
-
-
+    next_alarm = next_alarm + SNOOZE_TIME
+    print("new snooze set to:", next_alarm)
+    return
 
 def alarm_pooling():
     '''
@@ -175,28 +198,25 @@ def alarm_pooling():
     global next_alarm
 
     if not next_alarm:
-        next_alarm = get_next_alarm_for_today()
+        next_alarm = get_next_alarm()
         if not next_alarm:
-            print("there is no alarm")
+            print("there is no alarms in the xml file or the format is wrong")
             return
-
-    print(datetime.strptime(next_alarm.find('time').text, "%H:%M:%S").time())
+        print("next alarm set at:", next_alarm)
 
     # the time of the alarm has come
-    if datetime.strptime(next_alarm.find('time').text, "%H:%M:%S").time() <= datetime.now().time():
+    if next_alarm <= datetime.now():
         # rings the alarm
         ring_alarm()
-        next_alarm = get_next_alarm_for_today()
+    return
+
 
 
 def main():
-    print(get_next_alarm_for_today())
-
     while True:
         try:
             alarm_pooling()
-            time.sleep(0.5)
-
+            time.sleep(1)
         except Exception as e:
             print(e)
 
